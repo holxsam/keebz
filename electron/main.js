6080d210a7f52fd79382e480178914c582fcbe5b
at235am <at235am@gmail.com>
2021-12-17 16:53:46 -0800

    set up development environment for react + electron along with iohook

---
+++ electron/main.js
@@ -0,0 +1,100 @@
+const path = require("path");
+const url = require("url");
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  screen,
+  protocol,
+  Menu,
+} = require("electron");
+
+const ioHook = require("iohook");
+
+console.log("packaged?:", app.isPackaged);
+
+let mainWindow = null;
+
+ioHook.on("keydown", (key) => {
+  // console.log(keyNames[key.keycode]);
+  mainWindow.webContents.send("keydown", key.keycode);
+});
+
+ioHook.on("keyup", (key) => {
+  // console.log(keyNames[key.keycode]);
+  mainWindow.webContents.send("keyup", key.keycode);
+});
+
+// ioHook.on("mouseclick", (event) => {
+//   console.log("click");
+//   // mainWindow.webContents.send("mouseclick", event);
+// });
+
+ioHook.start();
+
+const createWindow = () => {
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, "../index.html"),
+      protocol: "file:",
+      slashes: true,
+    });
+  // console.log(process.env);
+  console.log({ startUrl });
+
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      // allowRunningInsecureContent: serve,
+      contextIsolation: false,
+      enableRemoteModule: true,
+      devTools: true,
+    },
+  });
+
+  // mainWindow.setAlwaysOnTop(true, "floating");
+  mainWindow.setVisibleOnAllWorkspaces(true);
+  mainWindow.setFullScreenable(false);
+
+  //load the index.html from a url
+  mainWindow.loadURL(startUrl);
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+};
+
+// IMPORTANT: option should be set to false to make it possible for ioHook global event
+// tracker boundaries works correctly.
+// app.allowRendererProcessReuse = false;
+
+app.on("ready", () => {
+  try {
+    createWindow();
+  } catch (error) {
+    ioHook.stop();
+    app.quit();
+  }
+});
+
+app.on("window-all-closed", () => {
+  // Quit when all windows are closed, except on macOS. There, it's common
+  // for applications and their menu bar to stay active until the user quits
+  // explicitly with Cmd + Q.
+  if (process.platform !== "darwin") {
+    ioHook.stop();
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import keyNames from "./utils/keycodes.json";
+
+const { ipcRenderer } = window; // this is possible because of the preload.js + main.js
+// other we would need to use `const { ipcRenderer } = window.require("electron");`
 
 function App() {
+  const [kbs, setKeys] = useState([]);
+
+  const addToKeys = (keycode) => {
+    setKeys((allKeys) => {
+      const index = allKeys.findIndex((v) => v === keycode);
+
+      if (index === -1) return [...allKeys, keycode];
+      else return allKeys;
+    });
+  };
+
+  const deleteFromKeys = (keycode) => {
+    setKeys((allKeys) => {
+      const index = allKeys.findIndex((v) => v === keycode);
+      return [
+        ...allKeys.slice(0, index),
+        ...allKeys.slice(index + 1, allKeys.length),
+      ];
+    });
+  };
+
+  useEffect(() => {
+    ipcRenderer.on("keydown", (e, m) => {
+      addToKeys(m);
+    });
+
+    ipcRenderer.on("keyup", (e, m) => {
+      deleteFromKeys(m);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {kbs.map((item, i) => (
+            <p key={i}>{keyNames[item]}</p>
+          ))}
+        </div>
+        <p onClick={() => console.log(keyNames)}>start typing to see keys</p>
       </header>
     </div>
   );
